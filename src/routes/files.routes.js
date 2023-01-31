f2b2b3cca729203dc2d7dca324b58a022229611b
leodeymison <leodeymisonbasquete@gmail.com>
2023-01-30 22:12:57 -0300

    start admin

--- src/routes/files.routes.js
+++ src/routes/files.routes.js
@@ -4,10 +4,22 @@ const path = require("path")
 const initial_path = require("../config/public")
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(initial_path, "home.html"))
+    res.sendFile(path.join(initial_path, "/html/home.html"))
 })
 router.get("/:id", (req, res) => {
-    res.sendFile(path.join(initial_path, "article.html"))
+    res.sendFile(path.join(initial_path, "/html/article.html"))
+})
+router.get("/admin/login", (req, res) => {
+    res.sendFile(path.join(initial_path, "/html/admin/login.html"))
+})
+router.get("/admin/article", (req, res) => {
+    res.sendFile(path.join(initial_path, "/html/admin/article-list.html"))
+})
+router.get("/admin/article/create", (req, res) => {
+    res.sendFile(path.join(initial_path, "/html/admin/article-create.html"))
+})
+router.get("/admin/article/edit/:id", (req, res) => {
+    res.sendFile(path.join(initial_path, "/html/admin/article-edit.html"))
 })
 
 module.exports = router;
